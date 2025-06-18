457bed33528773bed0f2ef48628e86255c164931
smajumdar22 <53415839+smajumdar22@users.noreply.github.com>
2025-06-18 13:12:56 -0700

    Create reversedPolishNotation.js

---
+++ PopularSDET/reversedPolishNotation.js
@@ -0,0 +1,29 @@
+const reversedPolishNotation = (tokens) => {
+  const stack = [];
+  
+  for (const token of tokens) {
+    if (!isNaN(token)) {
+      stack.push(Number(token));
+    } else {
+      const b = stack.pop();
+      const a = stack.pop();
+      
+      switch (token) {
+        case '+':
+          stack.push(a + b);
+          break;
+        case '-':
+          stack.push(a - b);
+          break;
+        case '*':
+          stack.push(a * b);
+          break;
+        case '/':
+          stack.push(a / b);
+          break;
+      }
+    }
+  }
+  
+  return stack[0];
+}
